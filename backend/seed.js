ad80352f31773d7171bed927a17bc052f0b56632
VincentRssx <Vincent.rssx59@gmail.com>
2024-02-01 14:19:33 +0100

    all push

--- backend/seed.js
+++ backend/seed.js
@@ -3,9 +3,6 @@
 // Load environment variables from .env file
 require("dotenv").config();
 
-// Import Faker library for generating fake data
-const { faker } = require("@faker-js/faker");
-
 // Import database client
 const database = require("./database/client");
 
@@ -19,17 +16,34 @@ const seed = async () => {
 
     // Generating Seed Data
 
-    // Optional: Truncate tables (remove existing data)
-    await database.query("truncate item");
+    await database.query("SET FOREIGN_KEY_CHECKS = 0");
+    await database.query("truncate InformationsGenerales");
+    await database.query("truncate ExperiencesProfessionnelles");
+    await database.query("truncate CompetencesTechniques");
+    await database.query("truncate FormationAcademique");
+    await database.query("SET FOREIGN_KEY_CHECKS = 1");
 
-    // Insert fake data into the 'item' table
-    for (let i = 0; i < 10; i += 1) {
-      queries.push(
-        database.query("insert into item(title) values (?)", [
-          faker.lorem.word(),
-        ])
-      );
-    }
+    queries.push(
+      database.query(`INSERT INTO InformationsGenerales (nom, prenom, adresse, telephone, email)
+VALUES ('Doe', 'John', '123 Main Street', '123-456-7890', 'john.doe@email.com');`)
+    );
+    queries.push(
+      database.query(`INSERT INTO ExperiencesProfessionnelles (titre, entreprise, lieu, date_debut, date_fin, description,user_id)
+VALUES ('Développeur Web', 'ABC Company', 'Ville A', '2020-01-01', '2022-01-01', 'Conception et développement de sites Web.',1);`)
+    );
+
+    queries.push(
+      database.query(`INSERT INTO CompetencesTechniques (domaine, competence, niveau,user_id)
+VALUES ('Langages de programmation', 'JavaScript', 'Avancé',1),
+       ('Frameworks', 'React', 'Intermédiaire',1),
+       ('Base de données', 'MySQL', 'Débutant',1);
+`)
+    );
+
+    queries.push(
+      database.query(`INSERT INTO FormationAcademique (diplome, etablissement, lieu, date_obtention,user_id)
+VALUES ('Baccalauréat en Informatique', 'Université XYZ', 'Ville B', '2019-01-01',1);`)
+    );
 
     /* ************************************************************************* */
 
